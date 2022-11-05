b8e2f34b73ccbac6e5c2bf51f6372dadcb55d9bd
nxoo <nxoo.dev@gmail.com>
2022-11-05 10:54:44 +0300

    models: user, review, product

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-    
+  has_many :reviews
+  has_many :products, through: :reviews
+
+  def favorite_product
+    reviews.order(:star_rating).last.product
+  end
+
+  def remove_reviews(product)
+    reviews.where(product: product).destroy_all
+  end
 end
